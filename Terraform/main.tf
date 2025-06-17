d2325c77fe2e36cf297e863e2790226f44b59faf
Sayn78 <87267809+Sayn78@users.noreply.github.com>
2025-06-17 15:44:10 +0200

    Add files via upload
    fichier main.tf pour Terraform (bpg) sur Proxmox

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,1 +1,44 @@
+  GNU nano 7.2                               main.tf                                        terraform {
+  required_providers {
+    proxmox = {
+      source  = "Telmate/proxmox"
+      version = ">=2.9.6"
+    }
+  }
+}
+
+provider "proxmox" {
+  pm_api_url      = "https://192.168.1.10:8006/api2/json"
+  pm_user         = "root@pam"
+  pm_password     = "exchg@03"
+  pm_tls_insecure = true
+}
+
+resource "proxmox_vm_qemu" "vm_web" {
+  name        = "web-vm"
+  target_node = "proxmox"
+  vmid        = 200
+  clone       = "ubuntu-cloudinit-template"
+  cores       = 2
+  memory      = 4096
+
+  disk {
+    size        = "20G"
+    type        = "scsi"
+    storage     = "local-lvm"
+  }
+  network {
+    model = "virtio"
+    bridge = "vmbr0"
+  }
+
+  os_type = "cloud-init"
+
+  ipconfig0 = "ip=192.168.1.200/24,gw=192.168.1.1"
+
+  sshkeys = file("~/.ssh/id_ed25519.pub")
+  ciuser = "senan"
+  cipassword = "exchg@03"
+}
+
 
